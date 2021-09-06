f25db4411271e74e014a2483401f8c2301f37a54
zaehuun <jdrjk0000@naver.com>
2021-09-07 08:08:02 +0900

    refactor: path를 절대 경로로 수정한다.
    경로를 절대 경로로 수정했습니다.

--- src/back/controller/index.js
+++ src/back/controller/index.js
@@ -1,9 +1,10 @@
 var express = require('express');
+const path = require('path');
 var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.sendFile(path.join('../../../dist/index.html'));
+  res.sendFile(path.join(__dirname,'dist','index.html'));
 });
 
 module.exports = router;
